3f0bc189ae9e80e84feea8cd9984e992b3d56a39
chrstphrriazo <80978203+chrstphrriazo@users.noreply.github.com>
2022-05-05 13:58:09 +0800

    Update MultipleAddress.sql

--- MultipleAddress.sql
+++ MultipleAddress.sql
@@ -1,14 +1,15 @@
-select
-p.FirstName,
-p.LastName,
-count(a.AddressID) as Count
-FROM Person.Person as p
-JOIN Person.BusinessEntity as be
-ON(be.BusinessEntityID=p.BusinessEntityID)
-JOIN Person.BusinessEntityAddress as bea
-ON(bea.BusinessEntityID=p.BusinessEntityID)
-JOIN Person.Address as a
-ON(a.AddressID=bea.AddressID)
-GROUP BY bea.BusinessEntityID, p.FirstName, p.LastName
-HAVING count(bea.AddressTypeID) > 1
-order by p.FirstName asc
+SELECT
+	p.FirstName,
+	p.LastName,
+	COUNT(a.AddressID) AS COUNT
+FROM 
+	Person.Person AS p
+	JOIN Person.BusinessEntity AS be ON(be.BusinessEntityID=p.BusinessEntityID)
+	JOIN Person.BusinessEntityAddress AS bea ON(bea.BusinessEntityID=p.BusinessEntityID)
+	JOIN Person.Address AS a ON(a.AddressID=bea.AddressID)
+GROUP BY 
+	bea.BusinessEntityID, p.FirstName, p.LastName
+HAVING 
+	COUNT(bea.AddressTypeID) > 1
+ORDER BY 
+	p.FirstName ASC
